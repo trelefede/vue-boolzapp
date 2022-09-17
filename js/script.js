10ffd4fcd721a34b0ee94071213a08075337862b
TreleaniFederico <treleani.federico@gmail.com>
2022-09-17 13:04:14 +0200

    add contacts and chats

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('js ok');
+console.log(contacts);
+
+
+const app = new Vue({
+    el: "#app",
+    data: {
+        contacts,
+        activeIndex: 0,
+    },
+    methods: {
+        setActiveContact(index) {
+            this.activeIndex = index;
+        }
+    }
+})
