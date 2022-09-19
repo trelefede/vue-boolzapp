d60d11c6118725201d251d3118e9b142d35f5727
TreleaniFederico <treleani.federico@gmail.com>
2022-09-19 17:33:55 +0200

    add sentMessage to js/html

--- js/script.js
+++ js/script.js
@@ -6,10 +6,25 @@ const app = new Vue({
     data: {
         contacts,
         activeIndex: 0,
+        textMessage: '',
     },
     methods: {
         setActiveContact(index) {
             this.activeIndex = index;
-        }
+        },
+        sentMessage() {
+            const messageSent = this.contacts[this.activeIndex].messages.push({
+                date: '10/01/2020 23:15:22',
+                message: this.textMessage,
+                status: 'sent'
+            });
+            setTimeout(() => {
+                const messageReceived = this.contacts[this.activeIndex].messages.push({
+                    date: '10/01/2020 23:15:22',
+                    message: 'OK!',
+                    status: 'received'
+                })
+            }, 3000);
+        },
     }
 })
