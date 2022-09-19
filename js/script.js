1450bfcb23d22eadae629ad6cc626cfd24e4d505
TreleaniFederico <treleani.federico@gmail.com>
2022-09-19 23:55:06 +0200

    add css style

--- js/script.js
+++ js/script.js
@@ -7,24 +7,33 @@ const app = new Vue({
         contacts,
         activeIndex: 0,
         textMessage: '',
+        searchInput: '',
     },
     methods: {
         setActiveContact(index) {
             this.activeIndex = index;
         },
         sentMessage() {
-            const messageSent = this.contacts[this.activeIndex].messages.push({
+            const activeContact = this.contacts[this.activeIndex];
+            activeContact.messages.push({
                 date: '10/01/2020 23:15:22',
                 message: this.textMessage,
                 status: 'sent'
             });
+            this.textMessage = '';
             setTimeout(() => {
-                const messageReceived = this.contacts[this.activeIndex].messages.push({
+                activeContact.messages.push({
                     date: '10/01/2020 23:15:22',
-                    message: 'OK!',
+                    message: 'Dai che è venerdi!',
                     status: 'received'
                 })
             }, 3000);
         },
+        getTimeMessage(dateLong) {
+            const array = dateLong.split(" ");
+            const hourMinuteSeconds = array[1].split(":");
+            const HourMinute = `${hourMinuteSeconds[0]}:${hourMinuteSeconds[1]}`;
+            return HourMinute;
+        }
     }
 })
